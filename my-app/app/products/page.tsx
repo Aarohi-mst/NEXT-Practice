b4b031c9d02a71b2cf304f51a746b8d396be1160
Aarohi-mst <aarohi.verma@metasquaretech.com>
2026-03-09 11:09:50 +0530

    Links and active links

--- my-app/app/products/page.tsx
+++ my-app/app/products/page.tsx
@@ -1,10 +1,15 @@
+import Link from "next/link";
+
 function page() {
+  const productId= 100;
   return (
     <div>
+      <Link href="/">Home</Link><br />
       <h1>Product lists</h1>
-      <h2>Product 1</h2>
-      <h2>Product 2</h2>
-      <h2>Product 3</h2>
+      <h2><Link href="/products/1">Product 1</Link></h2>
+      <h2><Link href="/products/2">Product 2</Link></h2>
+      <h2><Link href="/products/3" replace>Product 3</Link></h2>
+      <h2><Link href={`/products/${productId}`}>Product {productId}</Link></h2>
     </div>
   );
 }
